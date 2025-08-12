12a1836d760ca3ddf3e442bc8e780ba49875003a
Rashed Kamal <rashed.kamal@broadcom.com>
2025-08-12 16:03:43 -0400

    Update ko config with paketobuildpacks/run-jammy-tiny:latest remove woke scan bump go bump golangci-lint
    Signed-off-by: Rashed Kamal <rashed.kamal@broadcom.com>

--- tools/tools.go
+++ tools/tools.go
@@ -1,8 +1,9 @@
+//go:build tools
 // +build tools
 
 package tools
 
 import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
 )
